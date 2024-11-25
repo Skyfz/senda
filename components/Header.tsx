29ee1de5d3d5f6792456158a3957b77a72d3a9a6
Skyfz <mikesap34@gmail.com>
2024-11-26 01:06:42 +0200

    error_fix_commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
   return (
     <div className="flex items-center justify-between pb-4 pt-2">
       <div className="space-y-1">
-        <h1 className="text-white text-2xl font-bold">Hi&nbsp;{globalUser?.name}</h1>
-        <h2 className="text-white/80 text-sm font-medium">Welcome&nbsp;back</h2>
+        <h1 className="text-2xl font-bold">Hi,&nbsp;{globalUser?.name}</h1>
+        <h2 className="text-foreground/80 text-sm font-medium">Welcome&nbsp;back</h2>
       </div>
       <div className="flex items-center gap-6">
         {/* <AvatarDropdown className=""/> */}
-        <button className="p-2 rounded-full bg-white/10 backdrop-blur-md text-white hover:bg-white/20 transition-all">
+        <button className="p-2 rounded-full bg-black/5 dark:bg-white/5 backdrop-blur-md hover:bg-white/20 transition-all">
           <Bell size={30} />
         </button>
       </div>
