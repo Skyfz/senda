e62a7347adadfe13e92d4a8eac9310cdda476350
Skyfz <mikesap34@gmail.com>
2024-11-26 03:47:18 +0200

    error_fix_commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import { Bell } from 'lucide-react';
 import { useUser } from '@/context/user-context';
 import AvatarDropdown from './avatar-dropdown';
+import { Skeleton } from "@nextui-org/react";
 
 export default function Header() {
   const { globalUser } = useUser();
+
+  if (!globalUser) {
+    return (
+      <div className="flex items-center justify-between pb-6 pt-4 px-2">
+        <div className="space-y-2">
+          <Skeleton className="rounded-lg">
+            <div className="h-7 w-60"></div>
+          </Skeleton>
+          <Skeleton className="rounded-lg">
+            <div className="h-4 w-30"></div>
+          </Skeleton>
+        </div>
+        <div className="flex items-center gap-6">
+          <Skeleton className="rounded-full">
+            <div className="h-[46px] w-[46px]"></div>
+          </Skeleton>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex items-center justify-between pb-6 pt-4 px-2">
       <div className="space-y-1">
-        <h1 className="text-2xl font-bold">Hi,&nbsp;{globalUser?.name}</h1>
+        <h1 className="text-2xl font-bold">Hi,&nbsp;{globalUser.name}</h1>
         <h2 className="text-foreground/80 text-sm font-medium">Welcome&nbsp;back</h2>
       </div>
       <div className="flex items-center gap-6">
