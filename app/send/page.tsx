6e8fc6b86d3f332c9771247dba1d30a9cc1b01e9
Skyfz <mikesap34@gmail.com>
2024-12-26 23:17:59 +0200

    error_fix_commit

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -16,7 +16,7 @@ interface Contact {
     bio?: string;   //
 }
 
-export const ListboxWrapper = ({ children }: { children: ReactNode }) => (
+const ListboxWrapper = ({ children }: { children: ReactNode }) => (
   <div className="w-full px-1 py-2">
     {children}
   </div>
